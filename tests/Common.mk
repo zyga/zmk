bfade3c987dfba70a7d0bbc689b8210ac4ca7f9b
Zygmunt Krynicki <me@zygoon.pl>
2020-06-13 18:57:04 +0200

    Test log files ignore errors

--- tests/Common.mk
+++ tests/Common.mk
@@ -27,4 +27,4 @@ ZMK.makeTarget ?=
 		--warn-undefined-variables \
 		--always-make \
 		--dry-run \
-		$(or $(ZMK.makeTarget),$(firstword $(subst -, ,$*))) >$@ 2>&1)
+		$(or $(ZMK.makeTarget),$(firstword $(subst -, ,$*))) >$@ 2>&1) || true
