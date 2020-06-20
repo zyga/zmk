1f5c8ed06c73383e11a8818935b0a9176c0aeb5f
Zygmunt Krynicki <me@zygoon.pl>
2020-06-20 23:28:03 +0200

    Include "hello.h" in examples
    This shows how dependency tracking works.
    
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- examples/libhello-cpp/hello.cpp
+++ examples/libhello-cpp/hello.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "hello.h"
+
 void hello(void) {
 	std::cout << "Hello World" << std::endl;
 }
--- examples/libhello-objc/hello.m
+++ examples/libhello-objc/hello.m
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "hello.h"
+
 int hello(void) {
 	return printf("Hello World\n");
 }
