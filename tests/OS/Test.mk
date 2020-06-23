fc7a75aa94032eaf6e50fcfec87de23f387c67a8
Zygmunt Krynicki <me@zygoon.pl>
2020-06-23 20:24:49 +0200

    Use GREP with -x in OS tests
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- tests/OS/Test.mk
+++ tests/OS/Test.mk
@@ -10,53 +10,53 @@ t:: debug-linux debug-freebsd debug-openbsd debug-netbsd \
 
 debug-linux.log: ZMK.makeOverrides += OS.Kernel=Linux
 debug-linux: debug-linux.log
-	GREP -qF 'DEBUG: OS.Kernel=Linux' <$<
-	GREP -qF 'DEBUG: OS.ImageFormat=ELF' <$<
+	GREP -qFx 'DEBUG: OS.Kernel=Linux' <$<
+	GREP -qFx 'DEBUG: OS.ImageFormat=ELF' <$<
 
 debug-freebsd.log: ZMK.makeOverrides += OS.Kernel=FreeBSD
 debug-freebsd: debug-freebsd.log
-	GREP -qF 'DEBUG: OS.Kernel=FreeBSD' <$<
-	GREP -qF 'DEBUG: OS.ImageFormat=ELF' <$<
+	GREP -qFx 'DEBUG: OS.Kernel=FreeBSD' <$<
+	GREP -qFx 'DEBUG: OS.ImageFormat=ELF' <$<
 
 debug-openbsd.log: ZMK.makeOverrides += OS.Kernel=OpenBSD
 debug-openbsd: debug-openbsd.log
-	GREP -qF 'DEBUG: OS.Kernel=OpenBSD' <$<
-	GREP -qF 'DEBUG: OS.ImageFormat=ELF' <$<
+	GREP -qFx 'DEBUG: OS.Kernel=OpenBSD' <$<
+	GREP -qFx 'DEBUG: OS.ImageFormat=ELF' <$<
 
 debug-netbsd.log: ZMK.makeOverrides += OS.Kernel=NetBSD
 debug-netbsd: debug-netbsd.log
-	GREP -qF 'DEBUG: OS.Kernel=NetBSD' <$<
-	GREP -qF 'DEBUG: OS.ImageFormat=ELF' <$<
+	GREP -qFx 'DEBUG: OS.Kernel=NetBSD' <$<
+	GREP -qFx 'DEBUG: OS.ImageFormat=ELF' <$<
 
 debug-hurd.log: ZMK.makeOverrides += OS.Kernel=GNU
 debug-hurd: debug-hurd.log
-	GREP -qF 'DEBUG: OS.Kernel=GNU' <$<
-	GREP -qF 'DEBUG: OS.ImageFormat=ELF' <$<
+	GREP -qFx 'DEBUG: OS.Kernel=GNU' <$<
+	GREP -qFx 'DEBUG: OS.ImageFormat=ELF' <$<
 
 debug-gnu-kfreebsd.log: ZMK.makeOverrides += OS.Kernel=GNU/kFreeBSD
 debug-gnu-kfreebsd: debug-gnu-kfreebsd.log
-	GREP -qF 'DEBUG: OS.Kernel=GNU/kFreeBSD' <$<
-	GREP -qF 'DEBUG: OS.ImageFormat=ELF' <$<
+	GREP -qFx 'DEBUG: OS.Kernel=GNU/kFreeBSD' <$<
+	GREP -qFx 'DEBUG: OS.ImageFormat=ELF' <$<
 
 debug-solaris.log: ZMK.makeOverrides += OS.Kernel=SunOS
 debug-solaris: debug-solaris.log
-	GREP -qF 'DEBUG: OS.Kernel=SunOS' <$<
-	GREP -qF 'DEBUG: OS.ImageFormat=ELF' <$<
+	GREP -qFx 'DEBUG: OS.Kernel=SunOS' <$<
+	GREP -qFx 'DEBUG: OS.ImageFormat=ELF' <$<
 
 debug-darwin.log: ZMK.makeOverrides += OS.Kernel=Darwin
 debug-darwin: debug-darwin.log
-	GREP -qF 'DEBUG: OS.Kernel=Darwin' <$<
-	GREP -qF 'DEBUG: OS.ImageFormat=Mach-O' <$<
+	GREP -qFx 'DEBUG: OS.Kernel=Darwin' <$<
+	GREP -qFx 'DEBUG: OS.ImageFormat=Mach-O' <$<
 
 debug-windows.log: export OS=Windows_NT
 debug-windows: debug-windows.log
-	GREP -qF 'DEBUG: OS.Kernel=Windows_NT' <$<
-	GREP -qF 'DEBUG: OS.ImageFormat=PE' <$<
+	GREP -qFx 'DEBUG: OS.Kernel=Windows_NT' <$<
+	GREP -qFx 'DEBUG: OS.ImageFormat=PE' <$<
 
 debug-haiku.log: ZMK.makeOverrides += OS.Kernel=Haiku
 debug-haiku: debug-haiku.log
-	GREP -qF 'DEBUG: OS.Kernel=Haiku' <$<
-	GREP -qF 'DEBUG: OS.ImageFormat=ELF' <$<
+	GREP -qFx 'DEBUG: OS.Kernel=Haiku' <$<
+	GREP -qFx 'DEBUG: OS.ImageFormat=ELF' <$<
 
 debug-unknown.log: ZMK.makeOverrides += OS.Kernel=Unknown
 debug-unknown: debug-unknown.log
