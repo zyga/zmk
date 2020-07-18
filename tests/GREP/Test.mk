328da0921db319f120a238b4023f0a1d2e52a3f0
Zygmunt Krynicki <me@zygoon.pl>
2020-07-18 17:39:05 +0200

    Forward GREP to grep on case-insensitive filesystems
    When grep is invoked on a case-insensitive filesystem, and PATH contains
    the tests/bin directory, then GREP will be used, causing all kinds of
    confusion.
    
    Detect this case and redirect to system grep instead.
    
    While we are at it, add some tests for GREP.
    
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

---
+++ tests/GREP/Test.mk
@@ -0,0 +1,31 @@
+#!/usr/bin/make -f
+# This file is a part of zmk test system.
+include zmk/internalTest.mk
+
+t:: run-shellcheck GREP-pass GREP-fail GREP-redirect
+
+run-shellcheck:
+	# If we have shellcheck, then GREP should pass it without issues.
+	if [ -n "$$(command -v shellcheck)" ]; then shellcheck $(ZMK.Path)/tests/bin/GREP; fi
+
+GREP-pass:
+	# When GREP finds a match, the result is success.
+	echo "marry had a little lamb" | GREP lamb
+
+GREP-fail:
+	# When GREP dose not find a match, the result is failure.
+	! echo "marry had a little lamb" | GREP fox >/dev/null
+	# The failure is accompanied by details of the failure.
+	echo "marry had a little lamb" | GREP fox | grep -qFx "GREP: failed to match: grep 'fox'"
+	# If the input is a one-liner it is printed this way.
+	echo "marry had a little lamb" | GREP fox | grep -qFx "GREP: for input 'marry had a little lamb'"
+	# If the input is longer, it is printed verbatim.
+	printf "marry had\na little lamb\n" | GREP fox | grep -qFx "GREP: input starts below:"
+	printf "marry had\na little lamb\n" | GREP fox | grep -qFx "marry had"
+	printf "marry had\na little lamb\n" | GREP fox | grep -qFx "a little lamb"
+	printf "marry had\na little lamb\n" | GREP fox | grep -qFx "GREP: input ends above."
+
+GREP-redirect:
+	# GREP, if invoked as the lower-case grep, redirects back to system grep.
+	# Note that GREP describes itself as upper-case GREP in the usage string.
+	./grep 2>&1 | grep -q 'usage: grep'
