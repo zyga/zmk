5a248d996098965d37d6b0d4eb7e07512f755d66
Zygmunt Krynicki <me@zygoon.pl>
2020-08-16 01:36:51 +0200

    Fix escaping in GREP logs
    Escaping the dollar sign and double-quote was broken. This is now
    fixed and tested.
    
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- tests/GREP/Test.mk
+++ tests/GREP/Test.mk
@@ -16,14 +16,17 @@ GREP-fail:
 	# When GREP dose not find a match, the result is failure.
 	! echo "marry had a little lamb" | GREP fox >/dev/null
 	# The failure is accompanied by details of the failure.
-	echo "marry had a little lamb" | GREP fox | grep -qFx "GREP: failed to match: grep 'fox'"
+	echo "marry had a little lamb" | GREP fox | grep -qFx 'GREP: failed to match: grep "fox"'
 	# If the input is a one-liner it is printed this way.
-	echo "marry had a little lamb" | GREP fox | grep -qFx "GREP: for input 'marry had a little lamb'"
+	echo "marry had a little lamb" | GREP fox | grep -qFx 'GREP: for input "marry had a little lamb"'
 	# If the input is longer, it is printed verbatim.
 	printf "marry had\na little lamb\n" | GREP fox | grep -qFx "GREP: input starts below:"
 	printf "marry had\na little lamb\n" | GREP fox | grep -qFx "marry had"
 	printf "marry had\na little lamb\n" | GREP fox | grep -qFx "a little lamb"
 	printf "marry had\na little lamb\n" | GREP fox | grep -qFx "GREP: input ends above."
+	# The printed error output is correctly escaped.
+	echo '"this is one thing"' | GREP that | grep -qFx 'GREP: for input "\"this is one thing\""'
+	echo 'dollar is $$' | GREP cent | grep -qFx 'GREP: for input "dollar is \$$"'
 
 GREP-redirect:
 	# GREP, if invoked as the lower-case grep, redirects back to system grep.
