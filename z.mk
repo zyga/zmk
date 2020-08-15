754fb6b3575ba921dad0db2376d031fd501aa5fd
Zygmunt Krynicki <me@zygoon.pl>
2020-08-16 01:32:07 +0200

    Rewrite and fix Tarball.mk
    There are several changes that are bundled here.
    All the variables are now namespaced. Several things are defined
    as private variables that can be overridden for testing. GNU vs BSD
    tar is supported much better now, as is MacOS specific filtering.
    Lastly tarballs now work correctly when used out-of-tree.
    
    A tiny new feature is the option to never bundle files into the generated
    archive. This is also related to the change in how bundling is performed,
    removing the need to put a copy of zmk into the source tree.
    
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- z.mk
+++ z.mk
@@ -72,19 +72,6 @@ ZMK.manPages = \
 # Files belonging to ZMK that need to be distributed in third-party release tarballs.
 ZMK.DistFiles = z.mk $(addprefix zmk/,$(foreach m,$(ZMK.modules),$m.mk) pvs-filter.awk)
 
-# If zmk is provided externally add rules to copy it to the source tree and
-# make the distclean target remove it from the tree.
-ifneq ($(realpath $(ZMK.Path)),$(realpath $(srcdir)))
-$(srcdir)/zmk:
-	install -d $@
-$(srcdir)/zmk/%: $(ZMK.Path)/zmk/% | $(srcdir)/zmk
-	install -m 644 $< $@
-$(srcdir)/z.mk: $(ZMK.Path)/z.mk
-	install -m 644 $< $@
-distclean::
-	rm -rf $(srcdir)/zmk
-	rm -f $(srcdir)/z.mk
-	rm -f configure
 # The location of the source code.
 ZMK.SrcDir ?= .
 
--- zmk/Tarball.Src.mk
+++ zmk/Tarball.Src.mk
@@ -14,22 +14,26 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Zmk.  If not, see <https://www.gnu.org/licenses/>.
 
-
 %.asc: %
 		gpg --detach-sign --armor $<
+
+# Allow preventing ZMK from ever being bundled.
+ZMK.DoNotBundle ?= $(if $(value ZMK_DO_NOT_BUNDLE),yes)
 
 Tarball.Src.Variables=Name Files Sign
 define Tarball.Src.Template
 $1.Name ?= $$(patsubst %.tar$$(suffix $1),%,$1)
 $1.Files ?= $$(error define $1.Files - the list of files to include in the tarball)
-$1.Files += $$(ZMK.DistFiles)
+ifeq ($$(ZMK.DoNotBundle),)
+$1.Files += $$(addprefix $$(ZMK.Path)/,$$(ZMK.DistFiles))
+# If the Configure module is imported then include the configure script.
+ifneq (,$$(filter Configure,$$(ZMK.ImportedModules)))
+$1.Files += $(CURDIR)/configure
+endif
+endif
 # Sign archives that are not git snapshots and if CI is unset
 $1.Sign ?= $$(if $$(or $$(value CI),$$(and $$(filter GitVersion,$$(ZMK.ImportedModules)),$$(GitVersion.Active))),,yes)
 
-# If the Configure module is imported then include the configure script.
-ifneq (,$$(filter Configure,$$(ZMK.ImportedModules)))
-$1.Files += configure
-endif
 
 # If the GitVersion module is imported then attempt to insert version
 # information into the release archive. There are two possible cases.
