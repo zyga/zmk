d6dd7f4b72ce2e458d8aac002fd15b40a58faabc
Zygmunt Krynicki <me@zygoon.pl>
2021-09-07 18:17:56 +0200

    Add REUSE copyright and license headers
    In several files, add the missing LGPL boilerplate as well.
    
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- tests/Directories/Test.mk
+++ tests/Directories/Test.mk
@@ -1,4 +1,21 @@
 #!/usr/bin/make -f
+# SPDX-FileCopyrightText: 2019-2024 Zygmunt Krynicki
+# SPDX-License-Identifier: LGPL-3.0-only
+#
+# This file is part of zmk.
+#
+# Zmk is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# Zmk is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Zmk.  If not, see <https://www.gnu.org/licenses/>.
+
 include zmk/internalTest.mk
 
 t:: debug-defaults debug-name-defined debug-destdir debug-prefix \
--- zmk/pvs-filter.awk
+++ zmk/pvs-filter.awk
@@ -1,4 +1,5 @@
-# Copyright 2019-2024 Zygmunt Krynicki.
+# SPDX-FileCopyrightText: 2019-2024 Zygmunt Krynicki
+# SPDX-License-Identifier: LGPL-3.0-only
 #
 # This file is part of zmk.
 #
